44d65c5ed1309f3c2662859d88cf6dc94360d986
TheMoJd <moetez.jaoued@etudiant.univ-brest.fr>
2025-03-04 08:53:06 +0100

    init

---
+++ src/main/java/com/ubo/tp/message/MessageAppLauncher.java
@@ -0,0 +1,42 @@
+package com.ubo.tp.message;
+
+import com.ubo.tp.message.core.EntityManager;
+import com.ubo.tp.message.core.database.Database;
+import com.ubo.tp.message.core.database.IDatabase;
+import com.ubo.tp.message.ihm.MessageApp;
+import mock.MessageAppMock;
+
+/**
+ * Classe de lancement de l'application.
+ *
+ * @author S.Lucas
+ */
+public class MessageAppLauncher {
+
+	/**
+	 * Indique si le mode bouchoné est activé.
+	 */
+	protected static boolean IS_MOCK_ENABLED = true;
+
+	/**
+	 * Launcher.
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		IDatabase database = new Database();
+
+		EntityManager entityManager = new EntityManager(database);
+
+		if (IS_MOCK_ENABLED) {
+			MessageAppMock mock = new MessageAppMock(database, entityManager);
+			mock.showGUI();
+		}
+
+		MessageApp messageApp = new MessageApp(database, entityManager);
+		messageApp.init();
+		messageApp.show();
+
+	}
+}
